4913c07293b06bb99264b7c8d1d33723a9ec40df
johancs.mm@gmail.com <johancs.mm@gmail.com>
2023-12-01 22:54:55 -0500

    🚀 Add folder learn TypeScript

---
+++ learn/02-tsc-function.ts
@@ -0,0 +1,19 @@
+function great_world(name: string) {
+  return name;
+}
+
+great_world('10');
+
+function return_func(): number {
+  return 10;
+}
+
+return_func();
+
+// autocompletado
+
+let str = '10';
+
+str.toLowerCase();
+str.split(' ');
+str.slice(10);

9f7f9e36d0ee62d09314475b0a82d82a762fc41b
johancs.mm@gmail.com <johancs.mm@gmail.com>
2023-12-01 22:31:57 -0500

    🚀 Feature learn TypeScript

---
+++ learn/001-tsc-types.ts
@@ -0,0 +1,19 @@
+const tags_dev: string = "Tags Dev"
+
+const tags_number: number = 10
+
+const tags_boolean: boolean = true
+
+const tags_null: null = null
+
+const tags_array: Array<string> = ["", ""]
+
+const tags_array_version2: string[] = []
+
+let two_types_string_number: string | number = 10
+
+let array_two_type: (string | number)[] = [10, "10"]
+
+two_types_string_number = "10"
+
+console.log({tags_dev, tags_number, tags_boolean, tags_null, tags_array, tags_array_version2, two_types_string_number, array_two_type});

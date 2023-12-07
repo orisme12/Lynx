d7bd52f6e1654562321f8a919cb03a42ebbfbbfb
johancs.mm@gmail.com <johancs.mm@gmail.com>
2023-12-06 21:45:52 -0500

    🛠 Add config for vscode

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+/**
+ * This project is kept by:
+ * - Carlos bonet
+ * - Gerzon Rangel
+ * - Sebastian Garcia
+ * - Santiago Rico
+ * Mit License @copyright 2023
+ */
+
+import { example } from 'deps/const.ts'
+
+console.log({ example })
+
+console.log('Deno land 🦕')

37215fce499d83fc96ddaea21218ba7ba7286cd2
johancs.mm@gmail.com <johancs.mm@gmail.com>
2023-12-12 16:26:49 -0500

    🚀 Add example query to postgreSQL

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,25 @@
 import { Router } from 'packages/oak@v12.6.1/mod.ts'
+import adminInfo from '../admin.json' assert { type: 'json' }
+import queryToPostgreSQL from './database.ts'
+import { User } from 'types/mod.ts'
 
 const router = new Router()
 
 router.prefix('/api')
 
-router.get('/', (ctx) => {
-  ctx.response.body = 'Api Lynx'
+router.get('/', async (ctx) => {
+  const { data } = await queryToPostgreSQL<User[]>('SELECT * FROM users')
+
+  console.log(data)
+
+  ctx.response.body = {
+    name: 'Lynx Api',
+    version: adminInfo.version,
+    api: adminInfo.api,
+    description: adminInfo.description,
+    authos: adminInfo.authors,
+    data,
+  }
 })
 
 export default router

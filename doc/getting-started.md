29068305b6ce6f4983294efeba30b8b19cd59330
johancs.mm@gmail.com <johancs.mm@gmail.com>
2023-12-18 20:29:38 -0500

    Migrate deno to python

--- doc/getting-started.md
+++
@@ -1,4 +0,0 @@
-# Api official of Lynx
-
-Thank you for deciding to contribute to the official Lynx API 🚀. We are
-passionate about open sources and we have established rules
--- src/supabase.ts
+++
@@ -1,9 +0,0 @@
-import { createClient, load } from 'deps/lib.ts'
-
-const env = await load()
-const SUPABASE_URL = env['SUPABASE_URL']
-const SUPABASE_KEY = env['SUPABASE_KEY']
-
-const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
-
-export default supabase

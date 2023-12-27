ad0ce3140ecb63893f24a5c39010719e7597cd0f
johancs.mm@gmail.com <johancs.mm@gmail.com>
2023-12-26 22:14:01 -0500

    📦 Add permission ui

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
-# File principle
-
 from fastapi import FastAPI
 from app.routers.api import router_api
 from app.env import API_V1
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router_api, prefix=API_V1)

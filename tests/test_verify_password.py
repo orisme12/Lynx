0715814fbaa3a63c4e2c2d1e4b7d5e821dead59d
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-01-06 16:33:37 -0500

    🚀 Add test verify password

---
+++ tests/test_verify_password.py
@@ -0,0 +1,22 @@
+from app.deps import verify_password, get_password_hash
+
+
+def test_verify_password_is_valid():
+    test_password = "test-password"
+
+    hash_password = get_password_hash(test_password)
+
+    result = verify_password(test_password, hashed_password=hash_password)
+
+    assert result is True
+    assert result is not False
+
+
+def test_verify_password_is_not_valid():
+    test_password = "test-password"
+    hash_password = get_password_hash(test_password)
+
+    result = verify_password("not-password", hashed_password=hash_password)
+
+    assert result is False
+    assert result is not True

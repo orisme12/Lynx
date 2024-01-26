ec583aae1393fe09a2dfbb3735526963d0780852
castrogarciajs <johancs.mm@gmail.com>
2024-01-25 20:54:36 -0500

    Refactor function base

--- app/db/conn.py
+++ app/db/conn.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 
 
@@ -7,10 +7,8 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///../../commerce.db"
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+Base = sqlalchemy.orm.declarative_base()

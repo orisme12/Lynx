f3ab67d7aa88085e1c5a6dc5a95d0a1472074421
castrogarciajs <johancs.mm@gmail.com>
2024-01-25 20:55:04 -0500

    Test conn database sqlite

---
+++ tests/test_conn.py
@@ -0,0 +1,32 @@
+from app.db.conn import engine
+from sqlalchemy import text
+import pytest
+
+
+def test_connection_sqlite():
+    with engine.connect() as conn:
+        query = conn.execute(text("SELECT 'Hola, Mundo'"))
+
+        result = query.all()
+
+        assert isinstance(result, list)
+
+
+@pytest.mark.skip(reason="Table test alredy exits")
+def test_save_value():
+    with engine.connect() as conn:
+        conn.execute(text("CREATE TABLE test (x int, y int)"))
+        conn.execute(
+            text("INSERT INTO test (x, y) VALUES (:x, :y)"),
+            [{"x": 1, "y": 1}, {"x": 2, "y": 4}],
+        )
+
+        conn.commit()
+
+
+def test_returns_value():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT x, y FROM test"))
+        for row in result:
+            assert isinstance(row.x, int)
+            assert isinstance(row.y, int)

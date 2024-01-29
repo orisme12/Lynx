5dd3ba84f3a31999dd9651108676069b69699c64
castrogarciajs <johancs.mm@gmail.com>
2024-01-28 21:57:36 -0500

    Test migrate to SQL (#53)

--- tests/test_conn.py
+++ tests/test_conn.py
@@ -1,6 +1,23 @@
-from app.db.conn import engine
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 from sqlalchemy import text
+import os
 import pytest
+
+
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.environ.get("SQLALCHEMY_DATABASE_URL")
+
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, echo=True, connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
 
 
 @pytest.mark.skip(reason="Connection succesfuly")

e9a1d7fb15fdd03521ab11cd4947104c90dee53b
gerzon05 <gerzonarletrangel@gmail.com>
2024-02-02 22:56:24 -0500

    Added name validation in register

--- app/routers/v1/users.py
+++ app/routers/v1/users.py
@@ -64,7 +64,7 @@ async def register(user_credentials: types.UserCreate, db: Session = Depends(get
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="this email is not valid",
         )
-    
+
     if not user["name"] or len(user["name"]) == 0:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,

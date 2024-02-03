24e724e5d038727676c17249413e89ff55a8b05f
castrogarciajs <johancs.mm@gmail.com>
2024-02-03 16:34:10 -0500

    Fix error encode password

--- app/bcrypt.py
+++ app/bcrypt.py
@@ -4,8 +4,8 @@ salt = bcrypt.gensalt()
 
 
 def verify_password(plain_password: str, hashed_password: str):
-    enconde_password = plain_password.encode()
-    return bcrypt.checkpw(enconde_password, hashed_password)
+    enconde_password = plain_password.encode("utf-8")
+    return bcrypt.checkpw(enconde_password, hashed_password.encode("utf-8"))
 
 
 def get_password_hash(password: str):
